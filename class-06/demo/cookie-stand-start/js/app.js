ada6a0fa106a67e4c4dea9067644528b687a683a
Ryan Gallaway <rkgallaway@msn.com>
2021-05-12 19:14:30 -0700

    class demo added

--- class-06/demo/cookie-stand-start/js/app.js
+++ class-06/demo/cookie-stand-start/js/app.js
@@ -9,13 +9,27 @@ let seattle = {
   min: 23,
   max: 65,
   dailyTotal: 0,
-  avgCookiesSoldEachHourArray: [],
+  cookiesPerHourArray: [],
   avg: 6.3,
-  getRandomNumberCustomers: function () {
+  getCustomersPerHour: function () {
     return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  calcCookiesPerHour: function(){
+    // calc cookies for all 14 hours
+    // write a for loop that uses the length of the hours arary
+    //  in that loop
+    let custThisHour = this.getCustomersPerHour();
+    let cookiesSoldThisHour = Math.ceil(custThisHour * this.avg);
+    this.cookiesPerHourArray.push(cookiesSoldThisHour);
+    console.log('I am in the calcCookiesPerHour method', 'custThisHour: ', custThisHour, 'cookiesSoldThisHour: ', cookiesSoldThisHour);
+  },
+  render: function(){
+    this.calcCookiesPerHour();
+    console.log('this is the render method');
   }
+
   // enter your sudo code:
   // come up with a plan of how to do this!
 };
 
-console.log(seattle.getRandomNumberCustomers());
+seattle.render();
